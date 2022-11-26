1c8a9f70ae6561beb8da759d3cd02daacc532f88
Andrew Kassis <kassisaf@gmail.com>
2022-11-26 04:06:16 -0800

    Add dark mode support and toggle button

--- site/js/script.js
+++ site/js/script.js
@@ -1,7 +1,28 @@
-const preferredTheme = window.localStorage.getItem("theme");
+const body = document.querySelector("body");
 
-document.getElementById("dark-mode-btn").addEventListener("click", toggleDarkMode);
+const preferredTheme = window.localStorage.getItem("theme");
+if (preferredTheme) {
+    setDarkMode(preferredTheme == "dark");
+}
+else {
+    setDarkMode(true);
+}
+
+function setDarkMode(enable) {
+    if (enable) {
+        body.classList.add("dark");
+        body.classList.remove("light");
+        window.localStorage.setItem("theme", "dark");
+    }
+    else {
+        body.classList.add("light");
+        body.classList.remove("dark");
+        window.localStorage.setItem("theme", "light");
+    }
+}
 
 function toggleDarkMode() {
-
+    setDarkMode(body.classList.contains("light"));
 }
+
+document.getElementById("dark-mode-btn").addEventListener("click", toggleDarkMode);
