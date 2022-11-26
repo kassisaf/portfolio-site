f1dc6f0c785c139c98687c43b873d03307325c9a
Andrew Kassis <kassisaf@gmail.com>
2022-11-26 03:12:12 -0800

    Add colors for light theme

---
+++ site/js/script.js
@@ -0,0 +1,7 @@
+const preferredTheme = window.localStorage.getItem("theme");
+
+document.getElementById("dark-mode-btn").addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode() {
+
+}
