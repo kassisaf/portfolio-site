e4830d92753d477ee1553a21ba4a8c53a6a22140
Andrew Kassis <kassisaf@gmail.com>
2022-11-28 17:04:51 -0800

    Switch dark mode button between sun and moon icons

--- site/js/script.js
+++ site/js/script.js
@@ -4,6 +4,10 @@ const body = document.querySelector("body");
 const contentDiv = document.getElementById("main-content");
 const navExpand = document.getElementById("nav-expand");
 
+const darkModeIcon = document.getElementById("dark-mode-icon");
+const sun = `<circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>`;
+const moon = `<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>`;
+
 /* Dark mode */
 // Get dark mode preference
 const preferredTheme = window.localStorage.getItem("theme");
@@ -18,10 +22,12 @@ function setDarkMode(enable) {
     if (enable) {
         body.classList.add("dark");
         window.localStorage.setItem("theme", "dark");
+        darkModeIcon.innerHTML = sun;
     }
     else {
         body.classList.remove("dark");
         window.localStorage.setItem("theme", "light");
+        darkModeIcon.innerHTML = moon;
     }
 }
 
@@ -70,5 +76,5 @@ function setActive(targetSection) {
 }
 
 // Get home content on page load
-// loadFragment("content/home.html", "home");
-loadFragment("content/courses.html");
+loadFragment("content/home.html", "home");
+// loadFragment("content/courses.html");
