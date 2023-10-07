ff07acc378b33063711dfe52645b221edc377535
kassisaf <kassisaf@gmail.com>
2023-10-07 15:00:23 -0700

    Scroll to description when project card is clicked

--- site/js/script.js
+++ site/js/script.js
@@ -231,6 +231,7 @@ function createProjectLinkListeners() {
             // Destroy any existing project details before creating new one
             destroyProjectDetails();
             createProjectDetails(clickedProjectID);
+            currentProjectListItem.scrollIntoView({behavior: "smooth", block: "start"});
         });
     });
     // On browser resize, update project details span if present
